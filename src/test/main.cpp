f2a742aa0196434daa7fa92070ab394fc69962c1
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-04-29 13:23:18 +0200

    First functional build

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "../lib/libcanvas.h"
 
 int main()
 {
     std::cout << "libcanvas" << std::endl;
+
+    canvas * myCanvas = canvas_add_canvas("canvas");
+
+    canvas_set_active_canvas(myCanvas);
+
+    canvas_set_size(512, 512, 1);
 
     return 0;
 }
